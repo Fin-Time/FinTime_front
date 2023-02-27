71b0b3ea8ebb0726f8d7f330375f58c3b89968e3
wjdtjdgns <person3275@gmail.com>
2023-02-27 16:45:54 +0900

    Feat: Scheduler component 구현

--- src/App.js
+++ src/App.js
@@ -1,1 +1,100 @@
-export default function App({ $target }) {}
+import Scheduler from "./components/Scheduler.js";
+
+const DUMMY_DATA = {
+  monday: [
+    {
+      name: "헌법",
+      place: "사강404",
+      start_time: "9",
+      end_time: "12",
+    },
+    {
+      name: "인공지능과 미래사회",
+      place: "교307",
+      start_time: "13.5",
+      end_time: "15",
+    },
+  ],
+  tuesday: [
+    {
+      name: "노사관계론",
+      place: "경N411",
+      start_time: "10",
+      end_time: "12",
+    },
+    {
+      name: "자료구조",
+      place: "공5414",
+      start_time: "14",
+      end_time: "16",
+    },
+    {
+      name: "인공지능과 미래사회",
+      place: "공5410",
+      start_time: "16",
+      end_time: "17.5",
+    },
+  ],
+  wednesday: [
+    {
+      name: "자료구조",
+      place: "공5405",
+      start_time: "10",
+      end_time: "12",
+    },
+    {
+      name: "영미문예사조",
+      place: "인0465",
+      start_time: "13",
+      end_time: "15",
+    },
+    {
+      name: "열역학",
+      place: "공4414",
+      start_time: "16",
+      end_time: "18",
+    },
+  ],
+  thursday: [
+    {
+      name: "영미문예사조",
+      place: "인0465",
+      start_time: "10",
+      end_time: "11",
+    },
+    {
+      name: "노사관계론",
+      place: "경N411",
+      start_time: "11",
+      end_time: "12",
+    },
+    {
+      name: "열역학",
+      place: "공4414",
+      start_time: "13",
+      end_time: "14",
+    },
+    {
+      name: "동물생명정보분석및실습",
+      place: "농2202",
+      start_time: "16",
+      end_time: "18",
+    },
+  ],
+  friday: [
+    {
+      name: "동물생명정보분석및실습",
+      place: "농2202",
+      start_time: "15",
+      end_time: "17",
+    },
+  ],
+};
+
+export default function App({ $target }) {
+  new Scheduler({
+    $target,
+    initialState: DUMMY_DATA,
+    onDelete: (name) => {},
+  });
+}
---
+++ src/components/Scheduler.js
@@ -0,0 +1,142 @@
+export default function Scheduler({ $target, initialState, onDelete }) {
+  let isInitialize = true;
+  const $scheduler = document.createElement("table");
+  $target.appendChild($scheduler);
+  this.state = initialState;
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render();
+  };
+
+  const translateTimeToPosition = (time) => {
+    let position = 0;
+    for (let i = 10; i <= time; i++) {
+      position += 50;
+    }
+    return time % 1 !== 0 ? position + 25 : position;
+  };
+
+  const calcHeight = (start, end) => {
+    let height = 0;
+    for (let i = 0; i < Math.floor(end - start); i++) {
+      height += 50;
+    }
+    return !Number.isInteger(end - start) ? height + 25 : height;
+  };
+
+  const makeSubject = (
+    $target,
+    name,
+    place,
+    start_time,
+    end_time,
+    subjects
+  ) => {
+    const $div = document.createElement("div");
+    $target.appendChild($div);
+
+    if (subjects.includes(name)) {
+      $div.setAttribute("class", `subject color${subjects.indexOf(name) + 1}`);
+    } else {
+      $div.setAttribute("class", `subject color${subjects.length + 1}`);
+      subjects.push(name);
+    }
+
+    $div.setAttribute(
+      "style",
+      `top: ${translateTimeToPosition(start_time)}px; height: ${calcHeight(
+        start_time,
+        end_time
+      )}px;`
+    );
+
+    $div.innerHTML = `
+        <div class="subject_info">
+          <div class="subject_info__name">
+            <span>${name}</span>
+            <span class="delete">X</span>
+          </div>
+          <div class="subject_info__place">${place}</div>
+        </div>
+    `;
+  };
+
+  this.render = () => {
+    if (isInitialize) {
+      $scheduler.innerHTML = `
+        <tr>
+          <th></th><td>월</td><td>화</td><td>수</td><td>목</td><td>금</td>
+        </tr>
+        <tr>
+          <th>
+            <div class="times">
+              <div class="time">오전 9시</div>
+              <div class="time">오전 10시</div>
+              <div class="time">오전 11시</div>
+              <div class="time">오후 12시</div>
+              <div class="time">오후 1시</div>
+              <div class="time">오후 2시</div>
+              <div class="time">오후 3시</div>
+              <div class="time">오후 4시</div>
+              <div class="time">오후 5시</div>
+              <div class="time">오후 6시</div>
+              <div class="time">오후 7시</div>
+              <div class="time">오후 8시</div>
+            </div>
+          </th>
+          ${`<td class="days">
+              <div class="container"></div>
+              <div class="grids">
+                <div class="grid"></div>
+                <div class="grid"></div>
+                <div class="grid"></div>
+                <div class="grid"></div>
+                <div class="grid"></div>
+                <div class="grid"></div>
+                <div class="grid"></div>
+                <div class="grid"></div>
+                <div class="grid"></div>
+                <div class="grid"></div>
+                <div class="grid"></div>
+                <div class="grid"></div>
+              </div>
+            </td>`.repeat(5)}
+        </tr>
+      `;
+      isInitialize = false;
+    }
+    const subjects = [];
+    for (const key in this.state) {
+      this.state[key].map(({ name, place, start_time, end_time }) => {
+        let current;
+        switch (key) {
+          case "monday":
+            current = document.getElementsByClassName("days")[0].children[0];
+            break;
+          case "tuesday":
+            current = document.getElementsByClassName("days")[1].children[0];
+            break;
+          case "wednesday":
+            current = document.getElementsByClassName("days")[2].children[0];
+            break;
+          case "thursday":
+            current = document.getElementsByClassName("days")[3].children[0];
+            break;
+          case "friday":
+            current = document.getElementsByClassName("days")[4].children[0];
+            break;
+        }
+        makeSubject(current, name, place, start_time, end_time, subjects);
+      });
+    }
+  };
+
+  this.render();
+
+  $scheduler.addEventListener("click", (e) => {
+    const $scheduleDelete = e.target.closest(".delete");
+    if (!$scheduleDelete) return;
+    onDelete($scheduleDelete.parentNode.children[0].textContent);
+  });
+}
