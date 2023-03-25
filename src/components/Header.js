790d1363f82e3ee58e0e8a8f94ab512ef2f1f2c9
wjdtjdgns <person3275@gmail.com>
2023-03-26 01:46:49 +0900

    Design: Header 컴포넌트 기본 디자인

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,21 @@
 export default function Header({ $target }) {
   const $header = document.createElement("header");
+  $header.setAttribute("id", "header");
   $target.appendChild($header);
 
   this.render = () => {
     $header.innerHTML = `
-      <div id="header__anchors">
+      <div id="header__anchors" class="same-size">
         <a href="">시간표</a>
         <a href="">지도</a>
       </div>
-      <div id="header__logo">
+      <div id="header__logo" class="same-size">
         <img src="/src/images/logo.png" alt="Logo Image" />
       </div>
-      <div id="header__user"></div>
+      <div id="header__user" class="same-size">
+        <div id="header__user__name">test</div>
+        <button id="logout">Log out</button>
+      </div>
     `;
   };
 
