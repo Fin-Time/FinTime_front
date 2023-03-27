f0560c13fb1488ec73be0227de32294c5117d4b8
wjdtjdgns <person3275@gmail.com>
2023-03-27 13:07:11 +0900

    Design: Header 디자인 변경

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,17 +5,15 @@ export default function Header({ $target }) {
 
   this.render = () => {
     $header.innerHTML = `
+      <div id="header__logo" class="same-size">
+        <img src="/src/images/logo.png" alt="Logo Image" />
+      </div>
       <div id="header__anchors" class="same-size">
         <a href="">시간표</a>
         <a href="">지도</a>
       </div>
-      <div id="header__logo" class="same-size">
-        <img src="/src/images/logo.png" alt="Logo Image" />
-      </div>
-      <div id="header__user" class="same-size">
-        <div id="header__user__name">test</div>
-        <button id="logout">Log out</button>
-      </div>
+      <div class="same-size"></div>
+      
     `;
   };
 
